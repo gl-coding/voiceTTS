ac4ab04fed891cd2f0bb887770db61102e91ff7a
gl-coding <cool3stone@gmail.com>
2025-11-24 11:48:55 +0800

    更新数据库文件名为db.sqlite3.tts

--- project/tts_project/settings.py
+++ project/tts_project/settings.py
@@ -62,7 +62,7 @@ WSGI_APPLICATION = 'tts_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3.tts',
     }
 }
 
--- project/x_run.sh
+++ project/x_run.sh
@@ -2,11 +2,11 @@ arg=$1
 if [ "$arg" == "start" ]; then
     python manage.py runserver
 elif [ "$arg" == "backend" ]; then
-    pkill -f "python manage.py runserver"
+    pkill -f "python manage.py runserver 0.0.0.0:8001"
     nohup python manage.py runserver 0.0.0.0:8001 > /dev/null 2>&1 &
 elif [ "$arg" == "db" ]; then
     db_url=$2
-    curl -o "db.sqlite3" '$db_url'
+    curl -o "db.sqlite3.tts" '$db_url'
 elif [ "$arg" == "clean_media" ]; then
     rm -f media/audio/*
 fi
