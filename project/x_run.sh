3c335d499be8a635d0708cbe40870fc4ce4a87d1
gl-coding <cool3stone@gmail.com>
2025-11-24 11:02:59 +0800

    从Git中移除数据库文件并更新.gitignore

--- project/x_run.sh
+++ project/x_run.sh
@@ -1,1 +1,12 @@
-python manage.py runserver 8001
+arg=$1
+if [ "$arg" == "start" ]; then
+    python manage.py runserver
+elif [ "$arg" == "backend" ]; then
+    pkill -f "python manage.py runserver"
+    nohup python manage.py runserver 0.0.0.0:8001 > /dev/null 2>&1 &
+elif [ "$arg" == "db" ]; then
+    db_url=$2
+    curl -o "db.sqlite3" '$db_url'
+elif [ "$arg" == "clean" ]; then
+    rm -f media/audio/*
+fi
